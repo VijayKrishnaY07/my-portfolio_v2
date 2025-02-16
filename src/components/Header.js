48bb7b94a7204f60636f725be449ee496a4b9ad5
Vijay Krishna <vijaykrishnay07@gmail.com>
2025-02-15 16:53:19 -0800

    Added resume

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,7 +25,7 @@ function Header() {
     { label: "Contact", href: "#contact" },
     {
       label: "Resume",
-      href: "#contact",
+      href: "/Vijay_Krishna_Resume.pdf",
       external: true,
     },
   ];
